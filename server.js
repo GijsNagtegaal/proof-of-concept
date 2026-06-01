be33609c4a0cc15caba49b68ff92c9b3eacd45a1
Gijs <gijs.nagtegaal@hva.nl>
2026-06-01 16:55:00 +0200

    Init repository

--- server.js
+++ server.js
@@ -1,1 +1,70 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Import all npm packages
+
+import express from 'express';
+import { Liquid } from 'liquidjs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import methodOverride from 'method-override';
+
+// base directus URL 
+
+const api = "https://fdnd-agency.directus.app/items/"
+
+// setup the app
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views');
+app.set('view engine', 'liquid');
+
+app.get('/alle-huizen', async (req, res) => {
+    res.render('index.liquid', {
+        testje: "Funda"
+    });
+});
+
+// get the house detail page
+app.get('/huis/:house_slug', async (req, res) => {
+    try {
+        const response = await fetch(api + "f_houses");
+        const json = await response.json();
+        const houses = json.data;
+
+        // take what the user types in the url and make it the slug
+        const slug = req.params.house_slug.toLowerCase().trim();
+
+        // look for the house in the api database
+        const house = houses.find(house => {
+            return house.street && house.street.toLowerCase().trim() === slug;
+        });
+
+        if (!house) {
+            return res.status(404).render('404.liquid');
+        }
+
+        res.render('house-detail.liquid', { house: house });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+app.get('/', async (req, res) => {
+    res.render('index.liquid', {
+        testje: "Funda"
+    });
+});
+
+app.listen(8000, () => console.log('Server started: http://localhost:8000'));
+
